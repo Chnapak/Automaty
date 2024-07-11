59d5c902b5e8d10b790b2f709cd572c99ccff501
Chnapak <matej.hladecek@gmail.com>
2024-07-11 07:10:49 +0200

    Repository for Automaty created

---
+++ Automaty/Automat.cs
@@ -0,0 +1,23 @@
+﻿namespace Automaty
+{
+    internal class Automat
+    {
+        private string[] items = new string[3] { "sušenka", "bageta", "plechovka"};
+        private int[] prices = new int[3] {20, 35, 40};
+        
+
+        public string GetMeRandom()
+        {
+            var rand = new Random();
+            return items[rand.Next(items.Length)];
+        }
+        public int HowMuchCost(int index)
+        {
+            return prices[index];
+        }
+        public string GetMe(int index)
+        {
+            return items[0];
+        }
+    }
+}
